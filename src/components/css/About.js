ca68983703b1850038134b04b459cc5ea435ba04
zLuciel <82103687+zLuciel@users.noreply.github.com>
2023-02-03 19:59:33 -0500

    responsive about

--- src/components/css/About.js
+++ src/components/css/About.js
@@ -13,6 +13,22 @@ background: rgba(0, 0, 0, 0.25);
 backdrop-filter: blur(12.5px);
 border-radius: 15px;
 padding: 50px;
+//**Responsive */
+@media screen and (max-width: 1187px){
+ width: 800px;
+ height: 400px;
+}
+@media screen and (max-width: 852px){
+ width: 580px;
+}
+@media screen and (max-width: 628px){
+ width: 400px;
+ height: 340px;
+}
+@media screen and (max-width: 375px){
+ width: 300px;
+ height: 300px;
+}
  }
  .info-about{
     h1{
@@ -30,5 +46,23 @@ padding: 50px;
         font-size: 1.2rem;
         font-family: 'Unna';
     }
+//**Responsive */
+@media screen and (max-width: 628px){
+    h1{
+        font-size: 3rem;
+    }
+ p{
+    max-width: none;
  }
+}
+@media screen and (max-width: 375px){
+    h1{
+        font-size: 2rem;
+    }
+    p{
+        font-size: 1rem;
+    }
+ }
+
+}
 `
